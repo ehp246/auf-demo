66e7de54312af4f736899c92fa57315a26d2bcbe
Lei Yang <ehp246@yahoo.com>
2023-01-31 16:32:23 -0500

    Combined proxy.

--- src/main/java/me/ehp246/aufdemo/proxy/SendHeartbeat.java
+++ src/main/java/me/ehp246/aufdemo/proxy/SendHeartbeat.java
@@ -6,13 +6,17 @@ import org.springframework.scheduling.annotation.Scheduled;
 
 import me.ehp246.aufjms.api.annotation.ByJms;
 import me.ehp246.aufjms.api.annotation.ByJms.To;
+import me.ehp246.aufrest.api.annotation.ByRest;
+import me.ehp246.aufrest.api.annotation.OfRequest;
 
 /**
  * @author Lei Yang
  *
  */
-@ByJms(@To("auf-demo.bulletin"))
+@ByRest(value = "http://localhost:${server.port:8080}/heartbeat", name = "sendHeartbeatByRest")
+@ByJms(value = @To("auf-demo.bulletin"), name = "sendHeartbeatByJms")
 public interface SendHeartbeat {
+    @OfRequest(method = "POST")
     void heartbeat(Payload payload);
 
     @Scheduled(fixedRateString = "${auf-demo.heartbeat.delay:PT10S}")
--- src/main/java/me/ehp246/aufdemo/rs/Controller.java
+++ src/main/java/me/ehp246/aufdemo/rs/Controller.java
@@ -2,12 +2,15 @@ package me.ehp246.aufdemo.rs;
 
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import me.ehp246.aufdemo.inbox.proxy.Send;
+import me.ehp246.aufdemo.proxy.SendHeartbeat;
 
 /**
  * @author Lei Yang
@@ -15,11 +18,18 @@ import me.ehp246.aufdemo.inbox.proxy.Send;
  */
 @RestController
 public class Controller {
+    private final static Logger LOGGER = LogManager.getLogger();
+
     @Autowired
     private Send send;
 
     @PostMapping("/sum")
     public void sum(@RequestBody final List<Integer> ops) {
         send.sum(ops);
+    }
+
+    @PostMapping("/heartbeat")
+    public void heartbeat(@RequestBody final SendHeartbeat.Payload payload) {
+        LOGGER.atInfo().log("Heartbeat on {}", payload::instant);
     }
 }
